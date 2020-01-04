7b02a018651ef66ac6d70548cc0b8801c3635118
= <=>
2020-01-04 20:36:07 +0700

    Initial Commit

---
+++ Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finder
+//
+//  Created by Anthony Viriya on R 2/01/04.
+//  Copyright © Reiwa 2 AVL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
